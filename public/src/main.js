eaab400bbb6823e1e0fe1464fe86623a0bb616fb
zhoufei <zhoufei@imama.la>
2017-02-16 20:49:15 +0800

    重建了目录结构,晚上回去了开始做开发,用简单的路由开始做

--- public/src/main.js
+++ public/src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
+
+//公共配置及工具
+import vueRouter from 'vue-router'
+
+//页面组件
 import App from './App.vue'
+
+Vue.use(vueRouter);
+
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  components:{
+      app:App,
+  },
+  router:new vueRouter({
+      routes:[
+      {
+          path:'/',
+          component:App,
+      },
+      ]
+  }),
 })
